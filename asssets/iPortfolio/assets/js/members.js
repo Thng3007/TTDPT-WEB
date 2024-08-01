25d93c9da1aa9c59ecad1e4aa15267b6060626e5
Thanh Nguyen CTU <thng1701@gmail.com>
2024-08-01 14:47:45 +0700

    fix v5

--- asssets/iPortfolio/assets/js/members.js
+++ asssets/iPortfolio/assets/js/members.js
@@ -43,7 +43,7 @@ var members = {
 
 
         }
-    }
+    },
 
     //Nguyen Huu Hoa
     //Nguyen Nhi Gia Vinh
@@ -60,5 +60,50 @@ var members = {
     //Vu Duy Linh
     //Vo Hoang Dang
     //Nguyen Viet Thanh
+    "3006": {
+        "en": {
+            "home-code": "003006",
+            "home-name": " Dr. Nguyen Viet Thanh",
+            "home-slogan": "abcdefg",
+            "about-intro": "About Thanh",
+            "about-position": "Lecture",
+            "about-degree": "Doctorate in Computerscience & Engineering",
+            "about-seniority": 10,
+            "about-email": "vietthanh@ctu.edu.vn",
+            "about-conclusion": "Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis. Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.",
+            "about-faculty": "Faculty of Multimedia Communication - School of Infomation and Communication Technology",
+            "about-phone": "0123456789",
+            "about-studied": "Resume",
+            "about-studied-p": "Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.",
+            "about-portfolio": "Portfolio",
+            "about-courses": "Courses",
+            "about-courses-p": "He has responsibility to teach some courses such as:",
+
+
+        },
+        "vi": {
+            "home-code": "003006",
+            "home-name": "Dr. Nguyễn Viết Thanh",
+            "home-slogan": "abcdefg",
+            "about-position": "Giảng viên",
+            "about-degree": "Tiến sĩ",
+            "about-seniority": 10,
+            "about-email": "vietthanh@ctu.edu.vn",
+            "about-conclusion": "Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis. Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.",
+            "about-faculty": "Khoa Truyền Thông Đa Phương Tiện - Trường Công Nghệ Thông Tin - Trường Đại Học Cần Thơ",
+            "about-phone": "0123456789",
+            "about-studied": "Quá Trình Học Tập",
+            "about-studied-p": "Quá trình học abcdefghjklqeertyuiuiop",
+            "about-portfolio": "Một Số Hình Ảnh",
+            "about-courses": "Các Học Phần Giảng Dạy",
+            "about-courses-p": "Thầy đảm nhận giảng dạy một số học phần như sau:",
+
+
+
+
+
+
+        }
+    }
 
 }
