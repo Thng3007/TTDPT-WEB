7be18e26a0e87a330467e051ccd51fd5f3c843a5
Thanh Nguyen CTU <thng1701@gmail.com>
2024-08-09 09:40:50 +0700

    v13

--- asssets/iPortfolio/assets/js/common.js
+++ asssets/iPortfolio/assets/js/common.js
@@ -18,7 +18,6 @@ function showAva() {
     var code = urlParams.get('code'); // Assuming urlParams is defined
     var member = members[code][lang];
 
-
     // Set the src attribute for the first img element
     $('.img-fluid.my-avatar').attr('src', member['avatar']);
 
